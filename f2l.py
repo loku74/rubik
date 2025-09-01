e056e5936dc5d30344525683d8b78f91196a463f
loku74 <loicbn7@gmail.com>
2025-09-01 20:40:48 +0200

    f2l: top corners and edges check

--- f2l.py
+++ f2l.py
@@ -19,15 +19,38 @@ top_corners_dict = {
 
 
 def is_f2l_valid(cube: Cube):
-    def check_edge(cube: Cube):
+    def check_top_edge():
         edge_list = []
+
         for index in top_edges_index:
             top_color = cube.cube[Cube.YELLOW][index]
-            edge_face = top_edges_dict[index]
-            edge_color = cube.cube[edge_face][1]
+            edge_face_index = top_edges_dict[index]
+            edge_color = cube.cube[edge_face_index][1]
             if top_color != "Y" and edge_color != "Y":
                 edge_list.append((str(top_color), str(edge_color)))
+
         return edge_list
 
-    edges = check_edge(cube)
-    return edges
+    edge_pairs = check_top_edge()
+
+    def check_top_corners():
+        corner_list = []
+
+        for top_index, side_index_1, side_index_2 in top_corners_pairs:
+            top_color = cube.cube[Cube.YELLOW][top_index]
+
+            corner_face_index_1 = top_corners_dict[top_index][0]
+            corner_color_1 = cube.cube[corner_face_index_1][side_index_1]
+            corner_face_index_2 = top_corners_dict[top_index][1]
+            corner_color_2 = cube.cube[corner_face_index_2][side_index_2]
+
+            corner_colors = [str(top_color), str(corner_color_1), str(corner_color_2)]
+            if "W" in corner_colors:
+                corner_colors.remove("W")
+                corner_list.append(tuple(corner_colors))
+
+        return corner_list
+
+    top_pairs = check_top_corners()
+
+    return edge_pairs, top_pairs
